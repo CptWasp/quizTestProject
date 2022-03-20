a27b56cf25613d34afab805b5e7f68125178f3b8
BLRMYFC <StigNelson11@gmail.com>
2022-03-20 13:18:09 +0300

    create project

---
+++ src/main/java/com/quiz/quiz/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.quiz.quiz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class AdminController {
+
+    public String mainPage(Model model){
+
+//        model.addAttribute("quizList", repo)
+        return "";
+    }
+
+
+}
